84648a9957b31ab1a568c5cf620585cff11b52e8
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2026-04-30 10:09:19 +0200

    fix: helper

--- src/rat/commands/help.py
+++ src/rat/commands/help.py
@@ -1,20 +1,24 @@
 from .base_command import BaseCommand
 
+
 class HelpCommand(BaseCommand):
     name = "help"
 
     def execute(self, args: str) -> str:
         return (
             "help: Prints all the possible commands\n"
-                "download: Gather files from client and send them to server\n"
-                "upload: Gather files from server and send them to client\n"
-                "shell: Opens an interactive shell/bash/cmd\n"
-                "ipconfig: Get the client's network configuration\n"
-                "screenshot: Take a screenshot of the client\n"
-                "search: Search for a file on the client's filesystem\n"
-                "hashdump: Get the SAM or /etc/shadow file based on the OS\n"
-                "keylogger: Log every key of the client\n"
-                "webcam_snapshot: Take a picture using the client's webcam\n"
-                "webcam_stream: Livestream the client's webcam\n"
-                "record_audio: Record the client's audio\n"
+            "download: Gather files from client and send them to server\n"
+            "upload: Gather files from server and send them to client\n"
+            "shell: Opens an interactive shell/bash/cmd\n"
+            "ipconfig: Get the client's network configuration\n"
+            "screenshot: Take a screenshot of the client\n"
+            "search: Search for a file on the client's filesystem\n"
+            "hashdump: Get the SAM or /etc/shadow file based on the OS\n"
+            "keylogger: Log every key of the client\n"
+            "webcam_snapshot: Take a picture using the client's webcam\n"
+            "webcam_stream: Livestream the client's webcam\n"
+            "record_audio: Record the client's audio\n"
+            "exit : Exit the program and disconnect all clients\n"
+            "kill: Kill the client via his <id>\n"
+            "sessions: display all connected clients sessions\n"
         )
